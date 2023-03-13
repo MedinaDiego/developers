c857a457c75d8d21d35f161b0c6d17ad4f2a6b5d
Roberto Cortijo <developer.gsg@gmail.com>
2023-03-13 12:07:38 +0100

    Se crearon los metodos CRUD en TaskModel y TaskController

--- phpInitialDemo-main/app/controllers/TaskController.php
+++ phpInitialDemo-main/app/controllers/TaskController.php
@@ -10,18 +10,37 @@ class TaskController extends ApplicationController
 
     public function obtenerTasks(){
         $objTask = new TaskModel();
-        $tareas = $objTask->getTasks($_SESSION["sys_idUsuario"]);
+        $tareas = $objTask->getTasksByUser($_SESSION["sys_idUsuario"]);
         return $tareas;
     }
 
     public function nuevoTask(){
-
+        $inicio         = $_POST["txtinicio"];
+        $fin            = $_POST["txtfin"];
+        $nombre         = $_POST["txtnombre"];
+        $descripcion    = $_POST["txtdescripcion"];
+        $observaciones  = $_POST["txtobservaciones"];
+        
+        $objTask = new TaskModel();
+        $objTask->addTask($inicio, $fin, $nombre, $descripcion, $observaciones, $usuario);
     }
+
     public function editarTask(){
-
+        $idTarea        = $_POST["txtIdTarea"];
+        $inicio         = $_POST["txtinicio"];
+        $fin            = $_POST["txtfin"];
+        $nombre         = $_POST["txtnombre"];
+        $descripcion    = $_POST["txtdescripcion"];
+        $observaciones  = $_POST["txtobservaciones"];
+        $estado         = $_POST["txtestado"];
+        
+        $objTask = new TaskModel();
+        $objTask->editTask($idTarea, $inicio, $fin, $nombre, $descripcion, $observaciones, $estado);
     }
+    
     public function deleteTask(){
-
+        $objTask = new TaskModel();
+        $objTask->deleteTask($_POST["txtIdTarea"]);
     }
 
 }
