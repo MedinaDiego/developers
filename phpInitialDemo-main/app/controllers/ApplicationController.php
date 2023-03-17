ab9b6c5d8b2572821097b0b6a098871649239314
Diego Medina Mauricio <hacker290490@gmail.com>
2023-03-17 03:12:59 +0100

    añadimos los scripts en el header.

--- phpInitialDemo-main/app/controllers/ApplicationController.php
+++ phpInitialDemo-main/app/controllers/ApplicationController.php
@@ -6,5 +6,15 @@
  */
 class ApplicationController extends Controller 
 {
+    public function headerAction()
+	{
+        $tailwindcss ="https://cdn.tailwindcss.com?plugins=forms";
+        $jquery= "https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js";
+
+        $this->view->appendScript($tailwindcss);
+        $this->view->appendScript($jquery);
+
+        $this->view->printScripts(); 
+	}
 	
 }
