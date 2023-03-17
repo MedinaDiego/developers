154c7b0c9306e3dc19ddcb1456db2066b4b651d5
Diego Medina Mauricio <hacker290490@gmail.com>
2023-03-17 03:14:46 +0100

    Añadimos la funcion cerrar session, destroy

--- phpInitialDemo-main/app/controllers/LoginController.php
+++ phpInitialDemo-main/app/controllers/LoginController.php
@@ -1,30 +1,52 @@
-<?php 
-
-require_once (ROOT_PATH."/app/models/UserModel.class.php");
+<?php
+require_once(ROOT_PATH . "/app/models/UserModel.class.php");
 
 class LoginController extends ApplicationController
 {
-	public function uiLoginAction()
+    public function indexAction()
     {
-        //echo "";
+        $this->headerAction();
+        $url = $this->view->baseUrl();
+        $this->view->__set('ruta', $url);
     }
 
-    public function enviarDatosAction(){
-        $email= $_POST["email"];
-        $password = $_POST["password"];
-        
-        $objUser = new UserModel();
-        $usuario = $objUser->validateUser($email,$password);
+    public function validarAction()
+    {
+        $request = $this->getRequest();
 
-        if($usuario != null){
+        $email = $request->getParam("email", $default = null);
+        $password = $request->getParam("password", $default = null);
+
+        $objUser = new UserModel();
+        $usuario = $objUser->validateUser($email, $password);
+
+        if ($usuario != null) {
             $_SESSION["sys_idUsuario"] = $usuario["idUsuario"];
             $_SESSION["sys_nombre"] = $usuario["nombre"];
             $_SESSION["sys_mail"] = $usuario["mail"];
-            header("Location: ".WEB_ROOT."/main");
-        }else{
-            echo "<h2>Las credenciales son incorrectas.";
+            header("Location: " . WEB_ROOT . "/main");
+        } else {
+            echo "Error";
+           // $this->indexAction();
         }
+    }
+
+    public function cerrarAction()
+    {
+       
+        // Iniciamos la sesion
+        session_start();
+ 
+        // Destruir todo en esta sesión
+        session_destroy();
+
+        $this->view->disableView();
+        header("Location: " . WEB_ROOT . "/");
 
     }
 
+    public function checkAction()
+    {
+        echo "hello from test::check";
+    }
 }
